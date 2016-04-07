e70034d9975a4c8db9d5264830d2dfc5be6495e3
Tim Gremore <tim@entrision.com>
2016-04-07 06:43:43 -0500

    Restructure dom layout in About Us
    Width of each section varies so the '.primary-content' selector needs to
    be localized.

--- opploans2/content-about.php
+++ opploans2/content-about.php
@@ -2,34 +2,36 @@
   <div class=box>
     <h5 class="text-center">Family-owned, OppLoans believes you deserve better than payday loans. With faster funding, significantly lower rates, total transparency and unmatched customer service, Opploans is your opportunity to escape payday lending and improve your credit.</h5>
   </div>
+</div>
 
-  <div class='grid'>
-    <div class='grid__item small-block'>
-      <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
-        <li class='checkmark'>
-          <h5>Fast Funding.</h5>
-          <h6>Get instant approval today and money in your bank accout as early as tomorrow.</h6>
-        </li>
-      </ul>
-    </div>
-    <div class='grid__item small-block'>
-      <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
-        <li class='checkmark'>
-          <h5>Low Interest.</h5>
-          <h6>Never be subject to payday or origination fees, low loan amounts and rigid deadlines. OppLoans offers longer terms, higher loan amounts and APRs as much as 80% lower than payday lenders.</h6>
-        </li>
-      </ul>
-    </div>
-    <div class='grid__item small-block'>
-      <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
-        <li class='checkmark'>
-          <h5>Caring Service.</h5>
-          <h6>While other lenders push customers through automated approval processes, Opploans' highly-trained Loan Advocates combine amazing HUMAN customer service with our easy online application to deliver the money you need now!</h6>
-        </li>
-      </ul>
-    </div>
+<div class='grid'>
+  <div class='grid__item small-block'>
+    <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
+      <li class='checkmark'>
+        <h5>Fast Funding.</h5>
+        <h6>Get instant approval today and money in your bank accout as early as tomorrow.</h6>
+      </li>
+    </ul>
   </div>
+  <div class='grid__item small-block'>
+    <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
+      <li class='checkmark'>
+        <h5>Low Interest.</h5>
+        <h6>Never be subject to payday or origination fees, low loan amounts and rigid deadlines. OppLoans offers longer terms, higher loan amounts and APRs as much as 80% lower than payday lenders.</h6>
+      </li>
+    </ul>
+  </div>
+  <div class='grid__item small-block'>
+    <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
+      <li class='checkmark'>
+        <h5>Caring Service.</h5>
+        <h6>While other lenders push customers through automated approval processes, Opploans' highly-trained Loan Advocates combine amazing HUMAN customer service with our easy online application to deliver the money you need now!</h6>
+      </li>
+    </ul>
+  </div>
+</div>
 
+<div class="primary-content">
   <h5 class="text-center">Just right rates.</h5>
 
   <div class="grid">
