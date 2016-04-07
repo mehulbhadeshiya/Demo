96b43982d58027b8c2fe6bc6c83df39d7ae7114f
Tim Gremore <tim@entrision.com>
2016-04-07 07:25:39 -0500

    Layout of three highlights on about us page

--- opploans2/content-about.php
+++ opploans2/content-about.php
@@ -5,29 +5,26 @@
 </div>
 
 <div class='grid'>
-  <div class='grid__item small-block'>
-    <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
-      <li class='checkmark'>
-        <h5>Fast Funding.</h5>
-        <h6>Get instant approval today and money in your bank accout as early as tomorrow.</h6>
-      </li>
-    </ul>
+  <div class='grid__item small-block text-center'>
+    <div class='about-us__highlight mt pt'>
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Icon_FastFunding.svg" />
+    </div>
+    <h5 class=mt>Fast Funding</h5>
+    <h6>Get instant approval today and money in your bank accout as early as tomorrow.</h6>
   </div>
-  <div class='grid__item small-block'>
-    <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
-      <li class='checkmark'>
-        <h5>Low Interest.</h5>
-        <h6>Never be subject to payday or origination fees, low loan amounts and rigid deadlines. OppLoans offers longer terms, higher loan amounts and APRs as much as 80% lower than payday lenders.</h6>
-      </li>
-    </ul>
+  <div class='grid__item small-block text-center'>
+    <div class='about-us__highlight mt'>
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Icon_LowInterest.svg" />
+    </div>
+    <h5 class=mt>Low Interest.</h5>
+    <h6>Never be subject to payday or origination fees, low loan amounts and rigid deadlines. OppLoans offers longer terms, higher loan amounts and APRs as much as 80% lower than payday lenders.</h6>
   </div>
-  <div class='grid__item small-block'>
-    <ul class='checkmarks menu vertical menu-centered text-center grid_menu_small'>
-      <li class='checkmark'>
-        <h5>Caring Service.</h5>
-        <h6>While other lenders push customers through automated approval processes, Opploans' highly-trained Loan Advocates combine amazing HUMAN customer service with our easy online application to deliver the money you need now!</h6>
-      </li>
-    </ul>
+  <div class='grid__item small-block text-center'>
+    <div class='about-us__highlight mt pt'>
+      <img src="<?php echo get_template_directory_uri(); ?>/images/Icon_CaringService.svg" />
+    </div>
+    <h5 class=mt>Caring Service.</h5>
+    <h6>While other lenders push customers through automated approval processes, Opploans' highly-trained Loan Advocates combine amazing HUMAN customer service with our easy online application to deliver the money you need now!</h6>
   </div>
 </div>
 
