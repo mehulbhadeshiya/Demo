d89904e5d632215e824cd30e33369057e356ee2b
Ryan Cowan <rcowan@opploans.com>
2016-04-22 11:56:03 -0500

    homepage content updates

--- opploans2/footer.php
+++ opploans2/footer.php
@@ -98,7 +98,7 @@
             </p>
             <p>
               <small>
-                * Testimonials reflect the individual's opinion and may not be illustrative of all individual experiences with Opportunity Loans.
+                * Based on customer service ratings on Google and Facebook. Testimonials reflect the individual's opinion and may not be illustrative of all individual experiences with Opportunity Loans.
               </small>
             </p>
             <p>
