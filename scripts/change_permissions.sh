bfda5ef5fc30d17bfe6cfa8c3b8b563cea9efe18
abaid778 <abaid.gulshan@gmail.com>
2016-07-05 19:53:57 +0000

    change appspac.yml

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /root/wordpress 
 cp -r *  /var/www/html
-cd /home/ec2-user/independent-publisher/independent-publisher
-cp -r * /root/wordpress/wp-content/themes/independent-publisher
+cd /home/ec2-user/opploans2/opploans2
+cp -r * /root/wordpress/wp-content/themes/opploans2
 chown -R apache:apache /var/www/html
 chmod -R 744 /var/www/html
 
