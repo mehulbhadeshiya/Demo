049999e46e929988787eaf3c13241d3a36eb8f02
abaid778 <abaid.gulshan@gmail.com>
2016-07-29 20:54:40 +0500

    deployment script change added

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-cd /root/wordpress 
-cp -r *  /var/www/html
-cd /home/ec2-user/opploans2/opploans2
-cp -r * /root/wordpress/wp-content/themes/opploans2
-chown -R apache:apache /var/www/html
-chmod -R 744 /var/www/html
-
+cp -r * /var/www/html/site/wp-content/themes/opploans2
+cd /var/www/html/
+find . -type d -exec chmod 0755 {} \;
+find . -type f -exec chmod 0644 {} \;
+chown -R dev88c9:sftponly *
