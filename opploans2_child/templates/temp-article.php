cf3b786ca06d2538eb738212870fa67bc35669de
Mehul Bhadeshiya <mehulbhadeshiya@gmail.com>
2016-08-02 09:20:27 +0530

    test

--- opploans2_child/templates/temp-article.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Template name: Template name: Resource - Article
- */
-require_once ( dirname(__FILE__) . '/common-resource.php' );
-?>
--- opploans2_child/templates/temp-whitepapers.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/**
- * Template name: Resource - White paper
- */
-require_once ( dirname(__FILE__) . '/common-resource.php' );
-?>
